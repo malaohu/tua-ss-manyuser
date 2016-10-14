5d9c94dceba7e09c569f9fa3c4c4eb033317a8c4
malaohu <laohu_ma@163.com>
2016-10-14 17:22:08 +0800

    修改路径

--- shell/one_key_install_ss.sh
+++ shell/one_key_install_ss.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "install the required environment ..."
 MYDIR=`pwd`
-SSDIR= $MYDIR + "/a"
+SSDIR= $MYDIR"""/a"
 yum -y install git m2crypto
 wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 wget https://pypi.python.org/packages/source/p/pip/pip-6.0.7.tar.gz
@@ -13,8 +13,8 @@ pip install cymysql
 echo "download shadowsocks  ..."
 
 git clone -b master https://github.com/malaohu/tua-ss-manyuser $SSDIR
-chmod 775 $SSDIR/tua-ss-manyuser/shell/checkProcess.sh
-chmod 775 $SSDIR/tua-ss-manyuser/shell/ssstart.sh
+chmod 775 $SSDIR/shell/checkProcess.sh
+chmod 775 $SSDIR/shell/ssstart.sh
 echo "add check ss status shell ..."
 echo "*/30 * * * * bash "+ $SSDIR +"/shell/checkProcess.sh \"server.py\" \""+ $SSDIR +"/shell/ssstart.sh\"" >> /var/spool/cron/root
 vi $SSDIR/shadowsocks/Config.py
