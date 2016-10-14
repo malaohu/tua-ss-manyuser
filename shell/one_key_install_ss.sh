2718d02ffdbd703fbc1c4159dd53a85c54470148
malaohu <laohu_ma@163.com>
2016-10-14 16:40:00 +0800

    a

--- shell/one_key_install_ss.sh
+++ shell/one_key_install_ss.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 echo "install the required environment ..."
+MYDIR=`pwd`
 yum -y install git m2crypto
 wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 wget https://pypi.python.org/packages/source/p/pip/pip-6.0.7.tar.gz
@@ -10,13 +11,13 @@ pip install cymysql
 
 echo "download shadowsocks  ..."
 
-git clone -b master https://github.com/malaohu/tua-ss-manyuser /root/
-chmod 775 /root/tua-ss-manyuser/shell/checkProcess.sh
-chmod 775 /root/tua-ss-manyuser/shell/ssstart.sh
+git clone -b master https://github.com/malaohu/tua-ss-manyuser $MYDIR/a
+chmod 775 $MYDIR/a/tua-ss-manyuser/shell/checkProcess.sh
+chmod 775 $MYDIR/a/tua-ss-manyuser/shell/ssstart.sh
 echo "add check ss status shell ..."
-echo "*/30 * * * * bash /root/tua-ss-manyuser/shell/checkProcess.sh \"server.py\" \"/root/tua-ss-manyuser/shell/ssstart.sh\"" >> /var/spool/cron/root
-vi /root/tua-ss-manyuser/shadowsocks/Config.py
-echo "/root/tua-ss-manyuser/shell/ssstart.sh" >> /etc/rc.local
+echo "*/30 * * * * bash "+ $MYDIR +"/a/tua-ss-manyuser/shell/checkProcess.sh \"server.py\" \""+ $MYDIR +"/a/shell/ssstart.sh\"" >> /var/spool/cron/root
+vi $MYDIR/a/tua-ss-manyuser/shadowsocks/Config.py
+echo $MYDIR + "/a/tua-ss-manyuser/shell/ssstart.sh" >> /etc/rc.local
 
 echo "restart crond  ..."
 service crond reload
