fc9c9b7b9f439fbe78c99a9191a281c7096a5357
malaohu <laohu_ma@163.com>
2016-10-14 17:57:48 +0800

    完善一下

--- shell/one_key_install_ss.sh
+++ shell/one_key_install_ss.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "install the required environment ..."
 MYDIR=`pwd`
-SSDIR= $MYDIR"""/a"
+SSDIR=$MYDIR"""/a"
 yum -y install git m2crypto
 wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 wget https://pypi.python.org/packages/source/p/pip/pip-6.0.7.tar.gz
@@ -16,10 +16,12 @@ git clone -b master https://github.com/malaohu/tua-ss-manyuser $SSDIR
 chmod 775 $SSDIR/shell/checkProcess.sh
 chmod 775 $SSDIR/shell/ssstart.sh
 echo "add check ss status shell ..."
-echo "*/30 * * * * bash "+ $SSDIR +"/shell/checkProcess.sh \"server.py\" \""+ $SSDIR +"/shell/ssstart.sh\"" >> /var/spool/cron/root
+echo "*/30 * * * * bash """$SSDIR"""/shell/checkProcess.sh \"server.py\" \""""$SSDIR"""/shell/ssstart.sh """$SSDIR""\"" >> /var/spool/cron/root
 vi $SSDIR/shadowsocks/Config.py
 echo $SSDIR + "/shell/ssstart.sh" >> /etc/rc.local
 
 echo "restart crond  ..."
 service crond reload
 service crond restart
+
+$SSDIR/shell/ssstart.sh $SSDIR
--- shell/ssstart.sh
+++ shell/ssstart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 	
-cd /root/tua-ss-manyuser/shadowsocks
+cd $1/shadowsocks
 ulimit -n 51200
 nohup /usr/bin/python server.py >> /var/log/shadowsocks_$(date +%Y%m%d).log 2>&1 &
